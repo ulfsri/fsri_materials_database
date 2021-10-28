1762f666bc6d10f20be96e72c817b17b4124a234
cmccoy21 <90775214+cmccoy21@users.noreply.github.com>
2021-10-28 19:02:20 -0400

    Added files for Nylon carpet high and low pile, Polyester carpet high pile, and a redo for Eucalyptus floor. Note: need one more nylon high pile test

--- 02_Scripts/plot_MCC_data.py
+++ 02_Scripts/plot_MCC_data.py
@@ -296,8 +296,8 @@ for d in os.scandir(data_dir):
         f'{data_dir}{material}/MCC/{material}_MCC_Heats_of_Combustion.csv', float_format='%.2f')
 
 
-    # # trouble-shooting by outputting the dataframes
-    # if material == 'Polyester_Felt':
+    # trouble-shooting by outputting the dataframes
+    # if material == 'Nylon_Carpet_HighPile':
     #     data_df.to_csv(
     #     f'{data_dir}{material}/MCC/{material}_MCC_DataDF.csv', float_format='%.2f')
     #     corrected_data.to_csv(
