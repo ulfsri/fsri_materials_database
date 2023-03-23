ec70b64865bfd852be06331361b56de0dd457c21
Mckinnon <23501@global.ul.com>
2023-03-23 15:51:22 -0400

    mckinnonm: updated pictures for several materials and changes material.json for clarity on component materials

--- 02_Scripts/Utilities/reduce_image_sizes.py
+++ 02_Scripts/Utilities/reduce_image_sizes.py
@@ -11,7 +11,7 @@ import cv2
 import numpy as np
 
 # Define figure dir location
-img_dir = '../../01_Data/Rayon_Bamboo_Pillowcase/'
+img_dir = '../../01_Data/Exterior_Plywood/'
 
 #Define new image size
 width = 2000
