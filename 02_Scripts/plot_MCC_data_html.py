36686b5b2bf0543e9171d5d63ad544782a3bd3e5
Mckinnon <23501@global.ul.com>
2023-09-26 14:14:08 -0400

    mckinnonm: addressed error in MCC plotting script

--- 02_Scripts/plot_MCC_data_html.py
+++ 02_Scripts/plot_MCC_data_html.py
@@ -25,11 +25,7 @@ import git
 from scipy import integrate
 from pybaselines import Baseline, utils
 
-plot_all = False
-if not plot_all: 
-    print('plot_all is set to False, so any materials with existing html output files will be skipped')
-
-plot_all = False
+plot_all = True
 if not plot_all: 
     print('plot_all is set to False, so any materials with existing html output files will be skipped')
 
@@ -74,6 +70,17 @@ def plot_mean_data(df):
 
 
     return()
+
+def reindex_data(data, min_x, max_x, dx):
+    data = data.loc[~data.index.duplicated(), :]
+    data = data.reindex(data.index.union(np.arange(min_x, max_x+dx, dx)))
+    data = data.astype('float64')
+    data.index = data.index.astype('float64')
+    data = data.interpolate(method='cubic')
+
+    data = data.loc[np.arange(min_x, max_x+dx, dx)]
+
+    return data
 
 def format_and_save_plot(file_loc):
 
@@ -126,15 +133,15 @@ for d in sorted((f for f in os.listdir(data_dir) if not f.startswith(".") and f 
         output_exists = False
         for c in ['MCC_HRR', 'MCC_HoC', 'MCC_Ign_Temp']: 
             if mat_status_df.loc[material, c]: output_exists = True
-    if output_exists: 
-        # print(f'Skipping {material} MCC --- plot_all is False and output charts exist')
-        continue
+        if output_exists: 
+            # print(f'Skipping {material} MCC --- plot_all is False and output charts exist')
+            continue
 
     if os.path.isdir(f'{data_dir}{d}/MCC/'):
         fig = go.Figure()
         data_df = pd.DataFrame()
         plot_data_df = pd.DataFrame()
-        hoc_df = pd.DataFrame()
+        hoc_df_html = pd.DataFrame()
         all_col_names = []
         print(material + ' MCC')
         for f in glob.iglob(f'{data_dir}{d}/MCC/*.txt'):
@@ -151,78 +158,74 @@ for d in sorted((f for f in os.listdir(data_dir) if not f.startswith(".") and f 
 
                 if "O" not in col_name: # to ignore outliers (run code only for repetitions)
 
-                    all_col_names.append(col_name) # collect repetition numbers to account for botched tests (ex. R2, R3, R4, if R1 was bad)
+                    all_col_names.append(col_name) #
                     reduced_df = data_temp_df.loc[:,['Temperature (C)', 'HRR (W/g)']]
-                    reduced_df[f'time_{col_name}'] = reduced_df.index  #col_name[-1] to have the repetition number as -1 (not -R1) to help Regex later
-
-                    print(reduced_df)
-
+                    reduced_df[f'time_{col_name}'] = reduced_df.index 
 
                     # Correct from initial mass basis to mass lost basis
                     reduced_df['HRR (W/g)'] = reduced_df['HRR (W/g)']*(initial_mass/(initial_mass-final_mass))
 
                     max_lim = reduced_df['Temperature (C)'].iloc[-1] - ((reduced_df['Temperature (C)'].iloc[-1])%50)
-                    new_index = np.arange(150,int(max_lim)+1)
-                    new_data = np.empty((len(new_index),))
-                    new_data[:] = np.nan
-                    df_dict = {'Temperature (C)': new_index, 'HRR (W/g)': new_data}
-                    temp_df = pd.DataFrame(df_dict)
+                    # new_index = np.arange(150,int(max_lim)+1)
 
-                    # Resample data to every temperature
-                    reduced_df = pd.concat([reduced_df, temp_df], ignore_index = True)
-                    reduced_df.set_index('Temperature (C)', inplace = True)
-                    reduced_df.sort_index(inplace=True)
-                    reduced_df.interpolate(method='linear', axis=0, inplace=True)
-                    reduced_df = reduced_df.loc[new_index, :]
-
-                    reduced_df = reduced_df[~reduced_df.index.duplicated(keep='first')]
+                    reduced_df.set_index('Temperature (C)', inplace=True)
+                    reduced_df = reindex_data(reduced_df, 150, max_lim, 1)
 
                     # Baseline Correction
                     reduced_df['HRR correction'] = reduced_df.loc[150,'HRR (W/g)']+((reduced_df.index-150)/(reduced_df.index.max()-150))*(reduced_df.loc[reduced_df.index.max(),'HRR (W/g)'] - reduced_df.loc[150,'HRR (W/g)'])
                     reduced_df[col_name] = reduced_df['HRR (W/g)'] - reduced_df['HRR correction']
 
-                    data_df = pd.concat([data_df, reduced_df], axis = 1)
-                    data_array = data_df[col_name].to_numpy()
-                    time_array = data_df[f'Time_copy_{col_name[-1]}'].to_numpy() #col_name[-1] to have the repetition number of the time column as -1 (not -R1) to help Regex later
+                    data_array = reduced_df[col_name].to_numpy()
+                    time_array = reduced_df[f'time_{col_name}'].to_numpy()
                     data_array = data_array[~np.isnan(data_array)]
                     time_array = time_array[~np.isnan(time_array)]
-                    hoc_df.at['Heat of Combustion (MJ/kg)', col_name] = (integrate.trapz(y=data_array, x=time_array)) / 1000
+                    time_array = np.unique(time_array)
 
-                    # Alternative Baseline Correction
+                    hoc_df_html.at['Heat of Combustion (MJ/kg)', col_name] = (integrate.trapz(y=data_array, x=time_array)) / 1000
+
+                    # Alternative Baseline Correction (NOT CURRENTLY IMPLEMENTED)
                     x = reduced_df.index
                     baseline_fitter = Baseline(x_data=x)
                     f = reduced_df['HRR (W/g)']
 
-                    out = baseline_fitter.imodpoly(f, poly_order = 3, num_std = 1, max_iter = 1000, return_coef = True)
+                    out = baseline_fitter.imodpoly(f, poly_order = 2, num_std = 1, max_iter = 1000, return_coef = True)
                     g = out[0] # Baseline
                     h = f-g
                     reduced_df[f'{col_name}_alt'] = h
                     reduced_df.dropna(inplace=True)
                     data_df = pd.concat([data_df, reduced_df], axis = 1)
 
-        corrected_data = data_df.filter(regex = 'R[0-9]')
+                    data_array_alt = reduced_df[f'{col_name}_alt'].to_numpy()
 
-        # corrected_data.to_csv(f'{data_dir}{material}/{material}_MCC_corrected.csv')
+                    # hoc_df_html.at['Heat of Combustion [ALT] (MJ/kg)', col_name] = (integrate.trapz(y=data_array_alt, x=time_array)) / 1000
 
-        # plot_data_df.loc[:,'HRR_mean'] = corrected_data.mean(axis = 1)
-        # plot_data_df.loc[:,'HRR_std'] = corrected_data.std(axis = 1)
+        corrected_data = data_df.loc[:,all_col_names]
+
+        plot_data_df.loc[:,'HRR_mean'] = corrected_data.mean(axis = 1)
+        plot_data_df.loc[:,'HRR_std'] = corrected_data.std(axis = 1)
 
     else:
         continue
 
     # hoc_df_html = pd.DataFrame(index = ['Heat of Combustion (MJ/kg)'], columns = ['Mean', 'Std. Dev.'])
-    # hoc_df_html['Mean'] = hoc_df.mean(axis=1).round(decimals=2)
-    # hoc_df_html['Std. Dev.'] = hoc_df.std(axis=1).round(decimals=2)
+    hoc_df_html = hoc_df_html.round(decimals=2)
+    hoc_df_html['Mean'] = hoc_df_html.mean(axis=1).round(decimals=2)
+    hoc_df_html['Std. Dev.'] = hoc_df_html.std(axis=1).round(decimals=2)
 
     hoc_df_html.index.rename('Value',inplace=True)
     hoc_df_html = hoc_df_html.reset_index()
     hoc_df_html.to_html(f'{data_dir}{material}/MCC/{material}_MCC_Heats_of_Combustion.html',index=False,border=0)
     mat_status_df.loc[material, 'MCC_HoC'] = True
 
+    plot_mean_data(plot_data_df)
+
+    plot_dir = f'../03_Charts/{material}/MCC/'
+
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
     format_and_save_plot(f'{plot_dir}{material}_MCC_HRR.html')
     mat_status_df.loc[material, 'MCC_HRR'] = True
-
-mat_status_df.to_csv('Utilities/material_status.csv', index_label = 'material')
 
 print()
 
